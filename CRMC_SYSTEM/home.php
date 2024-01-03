84cb1dc5c6dc5c5819733d26a8fd838315d7758f
John Nico M. Edisan <nicxsician@gmail.com>
2024-01-03 09:56:06 +0800

    Add files via upload

---
+++ CRMC_SYSTEM/home.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UCRMC</title>
+    <link rel="stylesheet" href="css/home.css">
+    <link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Marko One' rel='stylesheet'>
+</head>
+<body>
+    
+    <div class="header">
+        <div class="left-head">
+            <div class="logo">
+                <img src="assets/crmc-logo.png" alt="">
+            </div>
+
+            <div class="navbar">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About School</a></li>
+                    <li><a href="#">Classes</a></li>
+                    <li><a href="#">Programs</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="right-head">
+            <a href="signup.php">Start Learning</a>
+        </div>
+    </div>
+
+    <!-- END OF HEADER -->
+
+    <!-- Start of Hero -->
+
+    <div class="hero">
+
+        <div class="left-hero">
+            <h5>Discover your Knowledge</h5>
+            <h1>Learn with <br> us. Easily</h1>
+            <p>Explore interests, discover diverse opportunities. Gain knowledge and practical skills in 
+                engaging sessions. Join us for hands-on learning experiences at our 
+                orientation session for prospective students</p>
+
+                <div class="button-container">
+                    <a href="#">Teacher</a>
+                    <a href="#">Students</a>
+                </div>
+        </div>
+
+
+
+
+        <div class="right-hero"></div>
+    </div>
+
+
+</body>
+</html>
