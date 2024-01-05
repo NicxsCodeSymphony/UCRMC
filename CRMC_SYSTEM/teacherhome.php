a2f60cf5a0212f26fa6d24742f902c2951541bbf
Lawrenz Xavier Carisusa <129159516+LawX22@users.noreply.github.com>
2024-01-05 21:00:05 +0800

    Add files via upload

---
+++ CRMC_SYSTEM/teacherhome.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href='https://fonts.googleapis.com/css?family=Rubik' rel='stylesheet'>
+    <link rel="stylesheet" href="css/teacherhome.css">
+    <title>Teacher | Homepage</title>
+</head>
+<body>
+    <!-- SIDEBAR -->
+    <div class="container">
+        <div class="sidebar">
+            <div class="logo">
+                <img src="assets/crmc-logo.png" alt="">
+                <h3>UCRMC</h3>
+            </div>
+
+            <div class="navbar">
+
+            </div>
+
+            <div class="bottom-nvb">
+                <div class="box-1"></div>
+                <div class="box-2"></div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- HEADER -->
+    <div class="header">
+        <div class="left-head">
+            <h1>TEACHER</h1>
+        </div>
+        <div class="right-head">
+            <p>Academic year:</p>
+            <div class="year">
+                <p>2024-25</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- PROFILE -->
+    <div class="account">
+
+    </div>
+
+    <!-- ACADEMIC DETAILS -->
+    <div class="acad-details">
+
+    </div>
+
+    <!-- TABLE CONTAINER -->
+    <div class="table-container">
+
+    </div>
+
+    <!-- ATTENDANCE -->
+    <div class="attendance">
+
+    </div>
+
+    <!-- EXAMINATION INFORMATION -->
+    <div class="exam-info">
+
+    </div>
+</body>
+</html>
