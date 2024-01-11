d1c6cb52c379e20fe616e28c019d46ae03d333f2
Lawrenz Xavier Carisusa <129159516+LawX22@users.noreply.github.com>
2024-01-11 17:47:09 +0800

    Update course.php

--- CRMC_SYSTEM/course.php
+++ CRMC_SYSTEM/course.php
@@ -35,7 +35,7 @@ include 'php/courseCommands.php';
             margin: 10px;
             font-size: 1.2rem;
             padding: 10px;
-            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.4);
             transition: transform 0.3s ease-in-out;
         }
 
